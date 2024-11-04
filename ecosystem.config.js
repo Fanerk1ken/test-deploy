9d56b51b01342871bf579ebe6798d984d74aa524
Egor <154514236+Fanerk1ken@users.noreply.github.com>
2024-11-04 22:30:29 +0300

    add config

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apps: [
+        {
+            name: 'test-deploy',
+            script: 'npm run start',
+            time: true,
+            log_date_format: 'DD.MM.YYYY HH:mm:ss',
+        },
+    ],
+};
